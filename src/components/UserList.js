17d14c854f60184bd4d692593f7b9f3b0dde5a5d
EstLopCor <estherlopezcordoncillo@gmail.com>
2021-03-03 18:08:48 +0100

    first commit

---
+++ src/components/UserList.js
@@ -0,0 +1,15 @@
+
+import User from './User';
+const UserList = props =>{
+    return (
+         <section>
+          <ul className="cards">
+            <li>
+            <User/>
+            </li>
+            
+          </ul>
+        </section>
+    )
+}
+export default UserList;
